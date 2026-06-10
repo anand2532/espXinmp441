cee944942741b4557388c031f1a3ec016a03da88
anand <anandmohan.amp@gmail.com>
2026-06-10 12:02:36 +0530

    Refactor I2S setup and audio processing in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,27 @@
 #define I2S_WS   25
 #define I2S_SCK  26
 #define I2S_SD   39
+#define I2S_PORT I2S_NUM_0
 
-#define SAMPLE_RATE 16000
+#define SAMPLE_RATE  16000
+#define BUFFER_LEN   256
+#define SERIAL_BAUD  921600
 
-void setup()
+int32_t samples[BUFFER_LEN];
+int16_t pcmBuf[BUFFER_LEN];
+
+void setupI2S()
 {
-    Serial.begin(115200);   // high speed
-
     i2s_config_t i2s_config =
     {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
         .sample_rate = SAMPLE_RATE,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-        .communication_format = I2S_COMM_FORMAT_I2S,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 8,
-        .dma_buf_len = 256,
+        .dma_buf_count = 4,
+        .dma_buf_len = BUFFER_LEN,
         .use_apll = false,
         .tx_desc_auto_clear = false,
         .fixed_mclk = 0
@@ -34,27 +38,29 @@ void setup()
         .data_in_num = I2S_SD
     };
 
-    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-    i2s_set_pin(I2S_NUM_0, &pin_config);
+    i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_PORT, &pin_config);
+    i2s_zero_dma_buffer(I2S_PORT);
+}
+
+void setup()
+{
+    Serial.begin(SERIAL_BAUD);
+    setupI2S();
 }
 
 void loop()
 {
-    int32_t sample32;
-    size_t bytesRead;
+    size_t bytesRead = 0;
 
-    i2s_read(
-        I2S_NUM_0,
-        &sample32,
-        sizeof(sample32),
-        &bytesRead,
-        portMAX_DELAY
-    );
+    i2s_read(I2S_PORT, (void *)samples, sizeof(samples), &bytesRead, portMAX_DELAY);
 
-    if(bytesRead == 4)
-    {
-        int16_t sample16 = sample32 >> 14;
-        Serial.write((uint8_t*)&sample16, 2);
-    }
+    int samplesRead = bytesRead / sizeof(int32_t);
+    if (samplesRead == 0)
+        return;
+
+    for (int i = 0; i < samplesRead; i++)
+        pcmBuf[i] = samples[i] >> 14;
+
+    Serial.write((uint8_t *)pcmBuf, samplesRead * sizeof(int16_t));
 }
-
